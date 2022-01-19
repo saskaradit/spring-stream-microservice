3ad92de5cc486577803b12b9900ec74309c6ddfd
saskaradit <saskaradit@gmail.com>
2022-01-19 14:16:54 +0700

    fix lombok

--- twitter-to-kafka/src/main/java/com/streamicroservice/twitter/to/kafka/config/TwitterToKafkaServiceConfigData.java
+++ twitter-to-kafka/src/main/java/com/streamicroservice/twitter/to/kafka/config/TwitterToKafkaServiceConfigData.java
@@ -12,4 +12,20 @@ import java.util.List;
 public class TwitterToKafkaServiceConfigData {
     private List<String> twitterKeywords;
     private String welcomeMessage;
+
+    public List<String> getTwitterKeywords() {
+        return twitterKeywords;
+    }
+
+    public void setTwitterKeywords(List<String> twitterKeywords) {
+        this.twitterKeywords = twitterKeywords;
+    }
+
+    public String getWelcomeMessage() {
+        return welcomeMessage;
+    }
+
+    public void setWelcomeMessage(String welcomeMessage) {
+        this.welcomeMessage = welcomeMessage;
+    }
 }
