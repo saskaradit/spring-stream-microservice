69a0c0f006613f4489036a5cefc25627a5fccfb9
saskaradit <saskaradit@gmail.com>
2022-01-24 16:51:14 +0700

    version check

--- config-server/src/main/java/com/streamicroservices/config/server/ConfigServer.java
+++ config-server/src/main/java/com/streamicroservices/config/server/ConfigServer.java
@@ -8,6 +8,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class ConfigServer {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServer.class,args);
+        SpringApplication.run(ConfigServer.class, args);
     }
 }
